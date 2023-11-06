463ada92b212c65ce49afd8fe48abab86d776e04
Maria Laranjeira <up202004453@fe.up.pt>
2023-11-06 02:02:40 +0000

    Documentations

--- src/bot.pl
+++ src/bot.pl
@@ -39,11 +39,24 @@ best_move([CP, CB, _, _, _, _, _], C, M) :-
         find_best_move(AP, -1, [], M)
     ).
 
-
+% parse_path(+Path, -Move)
+%
+% Converts the ending position of a path in a move
+%
+% @param Path
+% @param Move
 parse_path(M, SC-SR-EC-ER):-
     M = [[SC, SR] | Rest],
     last(Rest, [ER, EC]).
-  
+
+% find_best_move (+ValidPaths, +BestScore, +BestMove, -FinalMove)
+%
+%  Chooses the best move for the computer based on which one generates the higher score
+%
+% @param ValidPaths
+% @param BestScore
+% @param BestMove
+% @param FinalMove
 find_best_move([], _BS, BM, BM).    
 find_best_move([P | Rest], BS, BM, FM) :-
     calculate_path_score(P, S),
@@ -63,10 +76,13 @@ calculate_path_score(P, S) :-
     last(P, [R, C]),
     position_score(R, C, S). 
 
-%random_move(+GameState, +Card, -Move)
+% random_move(+GameState, +Card, -Move)
 %
-% 
-%    
+% Chooses a random valid move for the computer.
+%
+% @param GameState
+% @param Card
+% @param Move    
 random_move([CP, CB, _, _, _, _, _], C, M) :-
     valid_moves([_, CB, _, _, _, _, _], CP, C, AP),
     
@@ -76,14 +92,34 @@ random_move([CP, CB, _, _, _, _, _], C, M) :-
         random_member(M, AP)
     ).  
 
+%random_card(+Player, +WhiteCards, +BlackCards, -Card)
+%
+% Chooses a random card for the opponent
+%
+% @param Player
+% @param WhiteCards
+% @param BlackCards
+% @param Card
 random_card('w', _WC, BC, C):-
     random_member(C, BC).
 random_card('b', WC, _BC, C):-
     random_member(C, WC).
     
+%bot_traxit_move(+GameState, -NewGameState)
+%
+% Performs a computer move when the computer is in traxit
+%
+% @param GameState
+% @param NewGameState
 bot_traxit_move([CP, CB, R, WC, BC, WS, BS],  NGS):-
     random_traxit_move([CP, CB, R, WC, BC, WS, BS],  NGS).
-              
+
+%random_traxit_move(+GameState, -NewGameState)
+%
+% Chooses a random move for the computer when it is in traxit, and displays it.
+%
+% @param GameState
+% @param NewGameState              
 random_traxit_move([CP, CB, R, WC, BC, WS, BS],  NGS):-
     get_pawn_positions(CB, CP, Pos),
     random_member( [SR, SC], Pos),
